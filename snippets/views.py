bee389afa65da53e6f1c497aeedf6e8148bb8988
vivialprt <ivan.grigoryev502@gmail.com>
2020-10-20 14:14:31 +0300

    Tutorial 6. Using router

--- snippets/views.py
+++ snippets/views.py
@@ -5,17 +5,8 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
